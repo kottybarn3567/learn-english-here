9e1571dfd375680d42dd250e839a0fedd1be3139
kottybarn3567 <kaualopes3567@gmail.com>
2025-04-21 19:27:13 -0300

    adicionando o iframe escondido e colocando conteudo

--- 01-old-version/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector("#toggle-aside");
-const aside = document.querySelector("#aside");
-
-toggleButton.addEventListener("click", () => {
-    aside.classList.toggle("aside-fechado");
-});
